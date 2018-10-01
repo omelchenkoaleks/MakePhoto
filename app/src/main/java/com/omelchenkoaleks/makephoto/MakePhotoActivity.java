0e658ab696fb018204edb23e81308fcf9bd78543
Aleksandr Omelchenko <omelchenkoaleks@gmail.com>
2018-10-01 14:36:48 +0300

    Пока тест

--- app/src/main/java/com/omelchenkoaleks/makephoto/MakePhotoActivity.java
+++ app/src/main/java/com/omelchenkoaleks/makephoto/MakePhotoActivity.java
@@ -1,13 +1,167 @@
 package com.omelchenkoaleks.makephoto;
 
 import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.hardware.Camera;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
-public class MakePhotoActivity extends Activity {
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class MakePhotoActivity extends Activity implements SurfaceHolder.Callback,
+        View.OnClickListener, Camera.PictureCallback, Camera.PreviewCallback,
+        Camera.AutoFocusCallback {
+
+    private Camera camera;
+    private SurfaceHolder surfaceHolder;
+    private SurfaceView preview;
+    private Button shotBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // если хотим, чтобы приложение постоянно имело портретную ориентацию
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        // если хотим, чтобы приложение было полноэкранным
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        // и без заголовка
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_make_photo);
+
+        preview = findViewById(R.id.surfaceViewMain);
+
+        surfaceHolder = preview.getHolder();
+        surfaceHolder.addCallback(this);
+        surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        shotBtn = (Button) findViewById(R.id.makePhoto);
+        shotBtn.setText("Shot");
+        shotBtn.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        camera = Camera.open();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (camera != null) {
+            camera.setPreviewCallback(null);
+            camera.stopPreview();
+            camera.release();
+            camera = null;
+        }
+    }
+
+    @Override
+    public void onAutoFocus(boolean b, Camera camera) {
+        if (b)
+        {
+            // если удалось сфокусироваться, делаем снимок
+            camera.takePicture(null, null, null, this);
+        }
+    }
+
+    @Override
+    public void onPictureTaken(byte[] bytes, Camera camera) {
+        // сохраняем полученные jpg в папке /sdcard/CameraExample/
+        // имя файла - System.currentTimeMillis()
+        try
+        {
+            File saveDir = new File("/sdcard/CameraExample/");
+
+            if (!saveDir.exists())
+            {
+                saveDir.mkdirs();
+            }
+
+            FileOutputStream os = new FileOutputStream(String.format("/sdcard/CameraExample/%d.jpg", System.currentTimeMillis()));
+            os.write(bytes);
+            os.close();
+        }
+        catch (Exception e)
+        {
+        }
+
+        // после того, как снимок сделан, показ превью отключается. необходимо включить его
+        camera.startPreview();
+    }
+
+    @Override
+    public void onPreviewFrame(byte[] bytes, Camera camera) {
+        // здесь можно обрабатывать изображение, показываемое в preview
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        try {
+            camera.setPreviewDisplay(surfaceHolder);
+            camera.setPreviewCallback(this);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        Camera.Size previewSize = camera.getParameters().getPreviewSize();
+        float aspect = (float) previewSize.width / previewSize.height;
+
+        int previewSurfaceWidth = preview.getWidth();
+        int previewSurfaceHeight = preview.getHeight();
+
+        ViewGroup.LayoutParams lp = preview.getLayoutParams();
+
+        // здесь корректируем размер отображаемого preview, чтобы не было искажений
+        if (this.getResources().getConfiguration().orientation != Configuration.ORIENTATION_LANDSCAPE)
+        {
+            // портретный вид
+            camera.setDisplayOrientation(90);
+            lp.height = previewSurfaceHeight;
+            lp.width = (int) (previewSurfaceHeight / aspect);
+            ;
+        }
+        else
+        {
+            // ландшафтный
+            camera.setDisplayOrientation(0);
+            lp.width = previewSurfaceWidth;
+            lp.height = (int) (previewSurfaceWidth / aspect);
+        }
+
+        preview.setLayoutParams(lp);
+        camera.startPreview();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == shotBtn)
+        {
+            // либо делаем снимок непосредственно здесь
+            // 	либо включаем обработчик автофокуса
+
+            //camera.takePicture(null, null, null, this);
+            camera.autoFocus(this);
+        }
     }
 }
