c5941da08a28719258b8fa208f33933faf416742
Aleksandr Omelchenko <omelchenkoaleks@gmail.com>
2018-10-01 14:00:51 +0300

    Initial commit

---
+++ app/src/main/java/com/omelchenkoaleks/makephoto/MakePhotoActivity.java
@@ -0,0 +1,13 @@
+package com.omelchenkoaleks.makephoto;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MakePhotoActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_make_photo);
+    }
+}
